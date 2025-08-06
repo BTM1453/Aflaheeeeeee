9572ffe305ab7d80faa25b91e5cf081af82f52cc
Jazin <ddfilterbot01@gmail.com>
2025-08-06 18:39:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,8 +31,8 @@ files = glob.glob(ppath)
 
 pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
-def ping_loop():
-    while True:
+#def ping_loop():
+    ₹while True:
         try:
             r = requests.get(URL, timeout=10)
             if r.status_code == 200:
